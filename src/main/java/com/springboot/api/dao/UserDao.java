28bc25cb8edea7f569fb38e2a2e1676ce55525e7
Hakjun <hiatus9014@gmail.com>
2022-10-28 13:48:06 +0900

    UserController add,delete 추가

--- src/main/java/com/springboot/api/dao/UserDao.java
+++ src/main/java/com/springboot/api/dao/UserDao.java
@@ -2,9 +2,12 @@ package com.springboot.api.dao;
 
 import com.springboot.api.domain.User;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Component
 public class UserDao {
@@ -16,6 +19,13 @@ public class UserDao {
         this.dataSource = dataSource;
         this.jdbcTemplate = jdbcTemplate;
     }
+    private RowMapper<User> rowMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+            return user;
+        }
+    };
 
     public void add(User user) {
         this.jdbcTemplate.update("INSERT INTO users(id,name,password) values(?,?,?)",
@@ -23,5 +33,9 @@ public class UserDao {
     }
     public int deleteAll() {
         return this.jdbcTemplate.update("delete from users");
+    }
+    public User get(String id)  {
+        String sql = "SELECT * FROM users WHERE id =?";
+        return this.jdbcTemplate.queryForObject(sql, rowMapper, id);
     }
 }
