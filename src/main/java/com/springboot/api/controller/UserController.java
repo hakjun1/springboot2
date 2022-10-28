529093e393b7a82e87345184ce74e5a94e17fbf6
Hakjun <hiatus9014@gmail.com>
2022-10-28 15:10:57 +0900

    UserController 진행중

--- src/main/java/com/springboot/api/controller/UserController.java
+++ src/main/java/com/springboot/api/controller/UserController.java
@@ -2,16 +2,14 @@ package com.springboot.api.controller;
 
 import com.springboot.api.dao.UserDao;
 import com.springboot.api.domain.User;
+import com.springboot.api.domain.dto.MemberDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1")
 public class UserController {
     private final UserDao userDao;
 
@@ -21,8 +19,11 @@ public class UserController {
 
     @GetMapping("/user")
     public User addAndGet() throws SQLException {
-        userDao.add(new User("1", "hakjun", "1234"));
         return userDao.get("1");
+    }
+    @PostMapping("/user")
+    public String postMemberDto(@RequestBody User user) {
+        return user.toString();
     }
     @DeleteMapping("/user")
     public ResponseEntity<Integer> deleteAll() {
