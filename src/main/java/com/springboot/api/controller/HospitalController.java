a80ecd1a9b591f02784f87890c106e5937dd1461
Hakjun <hiatus9014@gmail.com>
2022-11-02 13:07:23 +0900

    HospitalController @GetMapping

---
+++ src/main/java/com/springboot/api/controller/HospitalController.java
@@ -0,0 +1,37 @@
+package com.springboot.api.controller;
+
+
+import com.springboot.api.dao.HospitalDao;
+import com.springboot.api.domain.Hospital;
+import com.springboot.api.domain.User;
+import com.springboot.api.domain.dto.UserRequestDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/hospital")
+public class HospitalController {
+    private final HospitalDao hospitalDao;
+
+    @Autowired
+    public HospitalController(HospitalDao hospitalDao) {
+        this.hospitalDao = hospitalDao;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Hospital> get(@PathVariable Integer id) {
+        try {
+            Hospital hospital = this.hospitalDao.findbyId(id);
+            return ResponseEntity
+                    .ok()
+                    .body(hospital);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
+
+
